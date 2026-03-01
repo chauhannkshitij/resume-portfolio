b159d5e36f12935409fb74cd4b231a5e0b4bfc30
Kshitij Chauhan <kshitij@example.com>
2026-03-01 13:44:28 +0530

    Corrected PDF path issue

--- script.js
+++ script.js
@@ -6,13 +6,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             .scrollIntoView({ behavior: 'smooth' });
     });
 });
-
-// Force PDF Download
-function downloadResume() {
-    const link = document.createElement("a");
-    link.href = "Kshitij_Chauhan_Resume.pdf";  // Exact filename
-    link.download = "Kshitij_Chauhan_Resume.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
